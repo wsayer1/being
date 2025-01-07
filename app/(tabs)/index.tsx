220717433ceeee6189e62eae14ffce41b4484c0c
William Sayer <wdsayer1@gmail.com>
2025-01-07 17:54:33 +1100

    test

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,7 +3,7 @@ import { Text, View, StyleSheet } from "react-native";
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Home</Text>
+      <Text style={styles.text}>Home Git</Text>
     </View>
   );
 }
